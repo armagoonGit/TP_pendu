756b5484715fc050bf5dcafa852afb0212b10df2
Vick <vick.foex@gmail.com>
2020-12-03 10:38:53 +0100

    ajout des bonne pratique c'est important !

--- gameRules.py
+++ gameRules.py
@@ -12,6 +12,12 @@ que reste a faire : si la lettre n'est pas bon chance sur 8
 from module import addLettre, printGuesWord
 
 def valideLettre(lettre):
+    """
+    Verifie que le parametre est bien une lettre seul.
+    Return1 : la lettre en majuscule 
+    Return2 : False si le parametre est incorect
+    """
+    
     if len(lettre) > 1 or len(lettre) < 1:
         return( False )
     
@@ -23,13 +29,25 @@ def valideLettre(lettre):
     return(lettre)
 
 def askLettre():
+    """
+    Demande a l'utilisateur une lettre jusuqu'a se qu'elle soit valide
+    return : la lettre valide en majuscule
+    """
+    
     lettre = input("Veuiller choisir une lettre : ")
+    
     while valideLettre(lettre) == False:
         lettre = input("Veuiller choisir une lettre valide : ")
-    return( lettre )
-        
+    
+    return( lettre )   
 
 def checkPresence(guesWord = "A____", word = "ARBRE",lettre = 'B') :
+    """
+    Verifie que la lettre est presente' dans le mot, la remplace si oui
+    sinon affiche un message.
+    Return : le mot a deviner avec ses nouvelle lettre si besoin
+    """
+    
     if lettre in word :
         for i in range( len( word ) ):
             if word[i] == lettre:
@@ -41,17 +59,29 @@ def checkPresence(guesWord = "A____", word = "ARBRE",lettre = 'B') :
     return(guesWord)
     
 def genWordUnder(word):
+    """
+    Genere un str de meme longeur que le parametre avec la premeire lettre 
+    visible, les autre sont des _
+    """
+    
     lenWord = len( word )
     guesWord = word[0] + '_'*(lenWord - 1)
     printGuesWord(guesWord)
     return(guesWord)
 
 def stopGame(guesWord):
+    """
+    Return false si le mot n'est pas encore devinr entierement
+    Sinon return true
+    """
     if '_' in guesWord:
         return(False)
     return(True)
 
 def endGame(guesWord):
+    """
+    Verifie en fin de partie si le joueur a trouve le mot ou non
+    """
     if stopGame(guesWord) == True :
         print("youplaOup bravo c'est gagner")
     else :
--- module.py
+++ module.py
@@ -18,13 +18,17 @@ def readWord():
     for el in wordDoc :
         wordList.append( el.rstrip('\n') )
         
-    wordDoc.close()
-    wordList.sort()
-    
+    wordDoc.close() 
     return(wordList)
 
 def sortList(wordList):
+    """
+    Trie une liste par taille de mot. et par ordre alphabetique
+    return : la liste trier 
+    """
+
     sieze = {}
+    wordList.sort()
 
     for el in wordList :
         lenEl = str( len( el ) )
@@ -42,17 +46,29 @@ def sortList(wordList):
     return(wordList)
 
 def pickWord(wordList):
+    """
+    renvoie une mot aletatoire dans un liste de mot passer en para
+    """
+
     rand = randint(0, len( wordList ) - 1 )
     return( wordList[ rand ] )
     
 
 def getWord():
+    """
+    renvoie un mot aleatoire du fichier de mot
+    """
+    
     wordList = readWord()
     wordList = sortList(wordList)
     word = pickWord(wordList)
     return( word )
     
 def addLettre(guesWord, lettre, index):
+    """
+    concatenation d'une str pour changer un char a l'index demander
+    """
+    
     guesWord = guesWord[:index] + lettre + guesWord[index + 1:]
     return(guesWord)
     
